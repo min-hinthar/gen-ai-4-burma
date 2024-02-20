137c3ca43dd820352e735f1bd545a55624c48721
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-02-19 20:30:57 -0800

    Root CSS color gradient updated, Free Credit updated to 10 per new user

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -97,7 +97,7 @@ return (
 const Card = ({ image }: { image: IImage }) => {
     return (
         <li>
-        <Link href={`/transformations/${image._id}`} className="collection-card">
+        <Link href={`/transformations/${image._id}`} className="collection-card shadow-2xl shadow-black">
             <CldImage
             src={image.publicId}
             alt={image.title}
@@ -105,7 +105,7 @@ const Card = ({ image }: { image: IImage }) => {
             height={image.height}
             {...image.config}
             loading="lazy"
-            className="h-52 w-full rounded-[10px] object-cover"
+            className="h-52 w-full rounded-[10px] object-cover shadow-lg shadow-dark-600"
             sizes="(max-width: 767px) 100vw, (max-width: 1279px) 50vw, 33vw"
             />
             <div className="flex-between">
